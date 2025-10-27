dbb20c7ec5a22403322a3dcbeba7f2fe3693fc6a
Vivek <62751076+viveksabale1998@users.noreply.github.com>
2025-10-27 20:21:30 +0530

    update

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+# main.py
+
+from qiskit import QuantumRegister , ClassicalRegister , QuantumCircuit
+from qiskit_aer import StatevectorSimulator, QasmSimulator, AerSimulator
+import matplotlib.pyplot as plt
+
 def main():
     print("Hello from quantum-computing-lab!")
+    print("Setting up a simple Qiskit quantum circuit...")
 
+    qreg_q = QuantumRegister(1,'q')
+    creg_c = ClassicalRegister(1,'c')
+
+    qc = QuantumCircuit(qreg_q,creg_c)
+
+    qc.h(qreg_q[0])
+    qc.measure(qreg_q,creg_c)
+    print(qc.draw("mpl"))
+
+    # M_simulator = QasmSimulator()
+
+    # M_simulator = StatevectorSimulator()
+    M_simulator = AerSimulator()
+
+    job = M_simulator.run(qc,shots=1024)
+    result = job.result()
+    print(result.get_counts(qc))
 
 if __name__ == "__main__":
     main()
